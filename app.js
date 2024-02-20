f938cda98ef39436c44467a3d692401e0e7b8f06
xaktarous <delali.abdallah@gmail.com>
2024-02-20 14:49:36 +0100

    phone-version

--- app.js
+++ app.js
@@ -4,9 +4,23 @@ const span =document.querySelector("span");
 const form =document.querySelector("form");
 
 
+form.addEventListener("submit",()=>{
+   if(/^[A-z]+[0-9]*[.]?[A-z]+[0-9]*@[A-z]+[.][a-z]+$/.test(input.value)){
+      span.style.display="none";
+      input.classList.remove("invalid");
+     localStorage.setItem("data",JSON.stringify(input.value))
+      form.action="success.html";
+     
+   }else{
+      e.preventDefault()
+      input.classList.add("invalid");
+      span.style.display="block";
+   }
+})
+
 button.addEventListener("click",(e)=>{
 
- if(/^[a-z]+[0-9]*[.][a-z]+[0-9]*@[a-z]+[.][a-z]+$/.test(input.value)){
+ if(/^[A-z]+[0-9]*[.]?[A-z]+[0-9]*@[A-z]+[.][a-z]+$/.test(input.value)){
     span.style.display="none";
     input.classList.remove("invalid");
    localStorage.setItem("data",JSON.stringify(input.value))
